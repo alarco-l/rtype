4efae0544afdad5c01521ee1b2a96da7d6f9160e
thomas <thomas.mengelatte@epitech.eu>
2014-12-27 11:07:10 +0100

    update rfcclient to heritate to irfc

--- R-Type/R-Type/RFCClient.h
+++ R-Type/R-Type/RFCClient.h
@@ -1,12 +1,43 @@
 # pragma once
 
-# include "IRFCClient.h"
-# include "Network/Client.h"
+# include "IRFC.h"
+//# include "Network/Client.h"
 
 # define ASocket	Network::Socket
 
-class RFCClient : public IRFCClient
+class RFCClient : public IRFC
 {
+private:
+	enum idPlayer
+	{
+		PLAYER1,
+		PLAYER2,
+		PLAYER3,
+		PLAYER4,
+		PLAYERMAX,
+	};
+	enum Monster
+	{
+		MONSTER1,
+		MONSTER2,
+		MONSTER3,
+		MONSTER4,
+		MONSTERMAX,
+	};
+	enum Weapon
+	{
+		WEAPON1,
+		WEAPON2,
+		WEAPON3,
+		WEAPONMAX
+	};
+	bool	_handshate;
+	bool	_isConnect;
+	short int	_id[4];
+	short int	_idClientCrash;
+	//RecvCommand _recvCmd;
+	ASocket	*_socket;
+
 public:
 	RFCClient();
 	~RFCClient();
@@ -38,6 +69,10 @@ public:
 		RECVCLIENTCRASH
 	};
 
+
+	void		setSocket(Socket &socket);
+	void		getData(Socket &socket);
+
 	void	sendHandshate(unsigned int idClient = 0); //6bytes
 	void	sendMove(const sf::Vector2f &pos, const sf::Vector2f &direction); // 9bytes
 	void	sendShoot(Weapon weapon, unsigned int idAmmo, const sf::Vector2f &pos, const sf::Vector2f &direction); // 12bytes
@@ -45,42 +80,18 @@ public:
 	void	sendHitMonster(unsigned int idMonster, const sf::Vector2f &posMonster); // 7bytes
 	void	sendKillMonster(unsigned int idMonster, const sf::Vector2f &posMonster); // 7btytes
 
-	void	recvCmd();
-	void	recvHandshate(); 
-	void	recvMove();  
-	void	recvShoot();  
-	void	recvCollision();  
-	void	recvHitMonster();  
-	void	recvKillMonster();
-	void	recvMonsterSpawn();
-	void	recvMonsterMove();
-	void	recvMonsterDestroy();
-	void	recvMonsterKillPlayer();
-	void	recvMonsterFire();
-	void	recvStartGame();
-	void	recvClientCrash();
-
-private:
-	enum idPlayer
-	{
-		PLAYER1,
-		PLAYER2,
-		PLAYER3,
-		PLAYER4,
-		PLAYERMAX,
-	};
-	enum Monster
-	{
-		MONSTER1,
-		MONSTER2,
-		MONSTER3,
-		MONSTER4,
-		MONSTERMAX,
-	};
-	bool	_handshate;
-	bool	_isConnect;
-	short int	_id[4];
-	short int	_idClientCrash;
-	RecvCommand _recvCmd;
-	ASocket	*_socket;
+	void	recvCmd(Socket &socket);
+	void	recvHandshate(Socket &socket);
+	void	recvMove(Socket &socket);
+	void	recvShoot(Socket &socket);
+	void	recvCollision(Socket &socket);
+	void	recvHitMonster(Socket &socket);
+	void	recvKillMonster(Socket &socket);
+	void	recvMonsterSpawn(Socket &socket);
+	void	recvMonsterMove(Socket &socket);
+	void	recvMonsterDestroy(Socket &socket);
+	void	recvMonsterKillPlayer(Socket &socket);
+	void	recvMonsterFire(Socket &socket);
+	void	recvStartGame(Socket &socket);
+	void	recvClientCrash(Socket &socket);
 };
