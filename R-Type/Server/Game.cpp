c88345d00e54726a0699bde35e7082b6184cb615
alarco-l <saw.darky@gmail.com>
2014-12-28 23:35:07 +0100

    Server modif

--- R-Type/Server/Game.cpp
+++ R-Type/Server/Game.cpp
@@ -16,7 +16,7 @@ void				Game::init()
 	_dlLoader->loadDLL("MonsterType1", "monsterType1");
 	_dlLoader->loadDLL("MonsterType2", "monsterType2");
 }
-#include <fstream>
+
 void				Game::update(::hpl::Clock &time)
 {
 	IMonster::Dir	dir;
@@ -24,36 +24,28 @@ void				Game::update(::hpl::Clock &time)
 	float			t;
 	int				i = 0;
 	static int nb = 0;
-	std::ofstream file("fichier.txt", std::ios_base::app);
 
 	for (std::vector<IMonster *>::iterator it = _monster.begin(); it != _monster.end(); ++it)
 	{
 		dir = (*it)->update();
-		if (_world.transformComponents[_idMonster[i]] != NULL && _world.movementComponents[_idMonster[i]] != NULL)
+		t = time.getElapsedTime().asFloat();
+		if (t == 0)
+			t = 0.02f;
+		direction.x = dir.x - _monsterDir[i].x;
+		direction.y = dir.y - _monsterDir[i].y;
+		_monsterDir[i].x += direction.x * t * 50;
+		_monsterDir[i].y += direction.y * t * 50;
+		if (nb == 0)
 		{
-			t = time.getElapsedTime().asFloat();
-			if (t == 0)
-				t = 0.02f;
-			direction.x = dir.x - _monsterDir[i].x;
-			direction.y = dir.y - _monsterDir[i].y;
-//			file << direction.x << "  " << direction.y << std::endl;
-			_monsterDir[i].x += direction.x * t * 80;
-			_monsterDir[i].y += direction.y * t * 80;
-			//direction = sf::Vector2f(dir.x, dir.y) - _world.transformComponents[_idMonster[i]]->position;
-			//file << direction.x << "  " << direction.y << std::endl << std::endl;
-			_world.movementComponents[_idMonster[i]]->direction = sf::Vector2f(direction.x, direction.y);
-			if (nb == 0)
-			{
-				RFC::MonsterMove	mm;
-				mm.idMonster = _idMonster[i];
-				mm.coord.posX = dir.x;
-				mm.coord.posY = dir.y;
-				mm.dir.dirX = direction.x;
-				mm.dir.dirY = direction.y;
-				mm.orientation = 180.0;
-				for (std::vector<RFC*>::iterator client = _rfcManager.rfc.begin(); client != _rfcManager.rfc.end(); ++client)
-					(*client)->sendMonsterMove(mm);
-			}
+			RFC::MonsterMove	mm;
+			mm.idMonster = _idMonster[i];
+			mm.coord.posX = dir.x;
+			mm.coord.posY = dir.y;
+			mm.dir.dirX = direction.x;
+			mm.dir.dirY = direction.y;
+			mm.orientation = 180.0;
+			for (std::vector<RFC*>::iterator client = _rfcManager.rfc.begin(); client != _rfcManager.rfc.end(); ++client)
+				(*client)->sendMonsterMove(mm);
 		}
 		++i;
 	}
@@ -64,13 +56,9 @@ void				Game::update(::hpl::Clock &time)
 
 void				Game::deleteMonster(int id)
 {
-	if (_world.transformComponents[_idMonster[id]]->position.x < -800)
-	{
 		_monster.erase(_monster.begin() + id);
 		_idMonster.erase(_idMonster.begin() + id);
 		_monsterDir.erase(_monsterDir.begin() + id);
-//		_world.renderComponents[_idMonster[id]] = NULL;
-	}
 }
 
 void				Game::run(::hpl::Clock &time)
@@ -78,10 +66,6 @@ void				Game::run(::hpl::Clock &time)
 	uint			id;
 
 	update(time);
-	for (unsigned i = 0; i < _monster.size(); ++i)
-	{
-		deleteMonster(i);
-	}
 	if (_frameRate.getElapsedTime().second() % 4)
 	{
 		if (_spawn1)
@@ -120,7 +104,7 @@ void				Game::spawnMonster(std::string const &name)
 {
 	IMonster		*instance = NULL;
 	IMonster::Dir	dir;
-	uint			id = 0;
+	static uint		id = 0;
 	float			x, y = 0;
 
 	x = static_cast<float>(rand() % 200 + 1000);
@@ -138,13 +122,9 @@ void				Game::spawnMonster(std::string const &name)
 		return;
 	instance->setSpawn(x, y);
 	_monster.push_back(instance);
-	id = _world.createEmptyEntity();
-	_ressource.loadTexture(instance->getTexture());
-	_world.addRenderComponent(id, ComponentFactory::createRenderComponent(_ressource.getTexture(instance->getTexture())));
-	_world.addTransformComponent(id, ComponentFactory::createTransformComponent(sf::Vector2f(1021, 728), sf::Vector2f(x, y), sf::Vector2f(0.05f, 0.20f)));
-	_world.addMovementComponent(id, ComponentFactory::createMovementComponent(50, sf::Vector2f(0, 0)));
 	_idMonster.push_back(id);
 	_monsterDir.push_back(dir);
+	++id;
 }
 
 void				Game::fire(int id)
@@ -153,11 +133,6 @@ void				Game::fire(int id)
 	IMonster::Dir	dir;
 
 	dir = _monster[id]->fire();
-	tmp = _world.createEmptyEntity();
-	_ressource.loadTexture(_monster[id]->getFire());
-	_world.addRenderComponent(tmp, ComponentFactory::createRenderComponent(_ressource.getTexture(_monster[id]->getFire())));
-	_world.addTransformComponent(tmp, ComponentFactory::createTransformComponent(sf::Vector2f(1021, 728), sf::Vector2f(dir.x, dir.y + 50), sf::Vector2f(0.01f, 0.01f)));
-	_world.addMovementComponent(tmp, ComponentFactory::createMovementComponent(50, sf::Vector2f(-8, 0)));
 	RFC::MonsterShoot	mm;
 	mm.idMonster = id;
 	mm.weapon = RFC::Weapon::W1;
